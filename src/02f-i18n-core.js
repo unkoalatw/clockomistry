ccc15488cd231da6be9ec1c3bb9d7bc824cc84df
GreenStoreTW <littletiger0815@gmail.com>
2026-04-18 15:25:16 +0800

    i18n: modularize language files and expand support (RU, ES, NAN, LA, FR, DE)

---
+++ src/02f-i18n-core.js
@@ -0,0 +1,13 @@
+// --- 國際化核心 (i18n Core) ---
+// 此檔案會整合所有從 02a-02e 載入的語系檔
+const I18N = {
+    'zh-TW': LANG_ZH_TW,
+    'en': LANG_EN,
+    'ja': LANG_JA,
+    'fr': LANG_FR,
+    'de': LANG_DE,
+    'ru': LANG_RU,
+    'nan': LANG_NAN,
+    'es': LANG_ES,
+    'la': LANG_LA
+};
